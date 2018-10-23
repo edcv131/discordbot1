871928aeff59ca53746e38c62cc1d4c3bd63d821
edcv131 <44343435+edcv131@users.noreply.github.com>
2018-10-23 05:17:54 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`احبك مرة`,"https://www.twitch.tv/dark_legend_1003")
+client.user.setGame()
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
